55f22c2a3c430a4cdc488b604dea5c54da2dfa8f
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2025-05-25 02:11:04 +0500

    Pages are created along with Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div>
-      <h1>Welcome to the React App</h1>
-      <p>This is a simple React application.</p>
-    </div>
-  )
+    <Route>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+
+      </Routes>
+    </Route>
+  );
 }
