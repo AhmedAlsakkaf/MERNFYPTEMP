067bbdf5ca50b21b90dc918564b61e297c57ddaf
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2025-05-28 04:36:34 +0500

    Middle Ware in the index.js js function is added.
    in the controller auth we just replaced the erro in the catch statement with the middle ware we created from the index.js
    
    finally customzed error handler in the utiles for customizeing the type of error for the catched one.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -11,6 +11,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json("user Created Successfully!");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -24,5 +24,15 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoute);
-
 app.use("/api/auth", authRotue);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    sucess: false,
+    statusCode,
+    message,
+  });
+});
